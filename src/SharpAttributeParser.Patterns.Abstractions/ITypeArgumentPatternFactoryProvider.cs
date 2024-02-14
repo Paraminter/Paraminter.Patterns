4e635eb188798d92302cfa6067772e2f6ef4c80c
ErikWe <erikwestberg7@gmail.com>
2024-02-14 19:18:16 +0100

    Added specific providers of factories of patterns matching reference types

---
+++ src/SharpAttributeParser.Patterns.Abstractions/ITypeArgumentPatternFactoryProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpAttributeParser.Patterns;
+
+using System;
+
+/// <summary>Provides factories of <see cref="IArgumentPattern{T}"/> matching <see cref="Type"/> arguments.</summary>
+public interface ITypeArgumentPatternFactoryProvider
+{
+    /// <summary>The factory handling creation of <see cref="IArgumentPattern{T}"/> matching non-nullable <see cref="Type"/> arguments.</summary>
+    public abstract INonNullableTypeArgumentPatternFactory NonNullable { get; }
+
+    /// <summary>The factory handling creation of <see cref="IArgumentPattern{T}"/> matching nullable <see cref="Type"/> arguments.</summary>
+    public abstract INullableTypeArgumentPatternFactory Nullable { get; }
+}
---
+++ src/SharpAttributeParser.Patterns/ArrayArgumentPatternFactoryProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace SharpAttributeParser.Patterns;
+
+using System;
+
+/// <inheritdoc cref="IArrayArgumentPatternFactoryProvider"/>
+public sealed class ArrayArgumentPatternFactoryProvider : IArrayArgumentPatternFactoryProvider
+{
+    private readonly INonNullableArrayArgumentPatternFactory NonNullable;
+    private readonly INullableArrayArgumentPatternFactory Nullable;
+
+    /// <summary>Instantiates a <see cref="ArrayArgumentPatternFactoryProvider"/>, providing factories of <see cref="IArgumentPattern{T}"/> matching array-valued arguments.</summary>
+    /// <param name="nonNullable">The factory handling creation of <see cref="IArgumentPattern{T}"/> matching non-nullable array-valued arguments.</param>
+    /// <param name="nullable">The factory handling creation of <see cref="IArgumentPattern{T}"/> matching nullable array-valued arguments.</param>
+    public ArrayArgumentPatternFactoryProvider(INonNullableArrayArgumentPatternFactory nonNullable, INullableArrayArgumentPatternFactory nullable)
+    {
+        NonNullable = nonNullable ?? throw new ArgumentNullException(nameof(nonNullable));
+        Nullable = nullable ?? throw new ArgumentNullException(nameof(nullable));
+    }
+
+    INonNullableArrayArgumentPatternFactory IArrayArgumentPatternFactoryProvider.NonNullable => NonNullable;
+    INullableArrayArgumentPatternFactory IArrayArgumentPatternFactoryProvider.Nullable => Nullable;
+}
