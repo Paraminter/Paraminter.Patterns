ca453033a203ae70355cbc5658013fc62e9f68f9
ErikWe <erikwestberg7@gmail.com>
2024-03-11 21:14:45 +0100

    Made the input to patterns some generic type, rather than object

--- src/Attribinter.Patterns.Core/IArgumentPattern.cs
+++ src/Attribinter.Patterns.Core/IArgumentPattern.cs
@@ -1,11 +1,12 @@
 ﻿namespace Attribinter.Patterns;
 
 /// <summary>Represents a pattern used to match attribute arguments.</summary>
-/// <typeparam name="T">The type of the attribute arguments matched by the pattern.</typeparam>
-public interface IArgumentPattern<T>
+/// <typeparam name="TIn">The type of the attribute arguments being matched.</typeparam>
+/// <typeparam name="TOut">The type of the matched attribute arguments.</typeparam>
+public interface IArgumentPattern<in TIn, TOut>
 {
     /// <summary>Attempts to match the provided argument to the pattern.</summary>
     /// <param name="argument">The argument that is matched to the pattern.</param>
     /// <returns>A <see cref="ArgumentPatternMatchResult{T}"/> describing the outcome.</returns>
-    public abstract ArgumentPatternMatchResult<T> TryMatch(object? argument);
+    public abstract ArgumentPatternMatchResult<TOut> TryMatch(TIn argument);
 }
